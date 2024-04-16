8fca0ffde0282b72c86a497e506e43f090011d0b
Chirkh <91262171+Chirkh@users.noreply.github.com>
2024-04-16 12:38:59 +0100

    Update Logistic_Regression.py

--- Supervised Algorithms/Logistic_Regression.py
+++ Supervised Algorithms/Logistic_Regression.py
@@ -74,9 +74,9 @@ class LogisticRegress:
         return pred
         
     
-    @staticmethod
-    def sigmoid(x):
-        return 1/(1+np.exp(-x))    
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))    
 
 '''
 #Testing on simple data set
